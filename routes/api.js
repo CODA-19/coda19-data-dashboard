53f6090836a88436d20c767569aff7503ac4abde
Louis Mullie <louis.mullie@gmail.com>
2020-07-25 11:54:27 -0400

    Initial import

---
+++ routes/api.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+const R = require('r-script')
+const csvParse = require('csv-parse')
+const express = require('express')
+const { exec } = require("child_process")
+const router = express.Router()
+
+router.get('/data', function(req, res, next) {
+  
+  const dummyData = fs.readFileSync('./test/data.json')
+  const dummyJson = JSON.parse(dummyData)
+  
+  exec("RScript ./scripts/meta.R -la", (error, stdout, stderr) => {
+    
+    if (error) {
+      console.log(`error: ${error.message}`)
+      res.status(500)
+      return
+    }
+    
+    csvParse(stdout, {columns: false, trim: true}, function(error, rows) {
+      
+      if (error) {
+        console.log(`error: ${error.message}`)
+        res.status(500)
+        return
+      }
+      
+      const plotConfig = {
+        "effectLabel": "Odds Ratio",
+        "vBar": 1,
+        "nTicks": 6,
+        "width": 500
+      }
+      
+      const plotData = [
+        {
+          "description": "Site",
+          "overrideLabel": "OR (95% CI)"
+        }
+      ]
+      
+      for (row of rows.slice(1)) {
+        plotData.push({
+          "description": row[0],
+          "effect": {
+            "effect": parseFloat(row[1]), 
+            "low": parseFloat(row[2]),
+            "high": parseFloat(row[3])
+          },
+          "markerSize": 0.5
+        })
+      }
+      
+      res.json({
+        "plotConfig": plotConfig,
+        "data": plotData
+      })
+      
+    })
+    
+  })
+  
+})
+
+module.exports = router
---
+++ scripts/meta.R
@@ -0,0 +1,23 @@
+#Load libraries
+library(readr)
+library(metafor)
+library(RCurl)
+
+my_data <- read.csv("./test/data.csv", dec = ",")
+
+effect_sizes <- escalc(n1i = n_controls, n2i = n_patients, m1i = mean_controls, m2i = mean_patients, 
+    sd1i = sd_controls, sd2i = sd_patients, data = my_data, measure = "SMD", APPEND=TRUE)
+
+summary_table <- summary(effect_sizes)[,c("study", "yi", "ci.lb", "ci.ub")]
+ma_model <- rma(yi, vi, data = effect_sizes)
+#forest(ma_model, slab = paste(my_data$study, as.character(my_data$year), sep = ", "))
+
+est <- ma_model[[2]][[1]]
+lci <- ma_model[[6]]
+uci <- ma_model[[7]]
+
+summary_estimate <- data.frame("Overall", est, lci, uci)
+names(summary_estimate) <- c("study", "yi", "ci.lb", "ci.ub")
+
+final_data <- rbind(summary_table, summary_estimate)
+cat(format_csv(final_data))
