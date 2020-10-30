4ab0c6c714dcff91e85c1f8cf858c28de3336320
Xuefei <xuefei.shi@mail.mcgill.ca>
2020-10-30 14:51:57 -0400

    Changed mean icon in legend from circle to line

--- client/src/components/legend.vue
+++ client/src/components/legend.vue
@@ -5,7 +5,7 @@
         <ul class="ToggleController">
           <li v-for="(site, x) in sites.slice(0,5)" >
             <label class="siteItem" v-model="highlight" @mousemove="$emit('update:highlight', site)" @mouseout="$emit('update:highlight', null)">
-              <span class="siteIcon" v-bind:style="{background:colors[x]}"></span>
+              <span class="siteIcon" :class="site ==='Mean' ? 'Mean' : ''" v-bind:style="{background:colors[x]}"></span>
               <span class="siteLabel">{{site ==="Mean" ? $t('meanTxt') : site}}</span>
             </label>
           </li>
@@ -16,7 +16,7 @@
         <ul class="ToggleController">
           <li v-for="(site, x) in sites.slice(5,10)">
             <label class="siteItem" v-model="highlight" @mousemove="$emit('update:highlight', site)" @mouseout="$emit('update:highlight', null)">
-              <span class="siteIcon" v-bind:style="{background:colors.slice(5,10)[x]}"></span>
+              <span class="siteIcon" :class="site ==='Mean' ? 'Mean' : ''"  v-bind:style="{background:colors.slice(5,10)[x]}"></span>
               <span class="siteLabel">{{site ==="Mean" ? $t('meanTxt') : site}}</span>
             </label>
           </li>
