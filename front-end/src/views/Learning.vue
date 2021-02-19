17e7ed0670023c70c77f278ce8ed5609c4de98c5
hamela <alexis.hamel@mail.mcgill.ca>
2021-02-19 09:20:49 -0500

    Add stub content for the learning page

--- front-end/src/views/Learning.vue
+++ front-end/src/views/Learning.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="mainContainer">
+  <v-container class="mainContainer">
+  <div class="learning">
+     <h1>{{$t('learningTitle')}}</h1>
   </div>
+    <div>{{$t('startLearning')}}</div>
+  </v-container>
 </template>
 
 <script>
