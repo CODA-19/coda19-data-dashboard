5d542b98922b974b594e7c62485b32cb8cc686e4
xshi <xuefei.shi@mail.mcgill.ca>
2021-02-22 14:13:08 -0500

    use Echarts 5, added legend in home page - in progress

--- front-end/src/components/legend.vue
+++ front-end/src/components/legend.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="legendContainer">
+  <div :class="'legendContainer '+direction">
 
-      <div>
+      <div :class="direction==='horizontal'? 'col-lg-6 col-md-12 col-sm-12':''" >
         <ul class="ToggleController">
           <li v-for="(site, x) in sites.slice(0,5)" >
             <label class="siteItem" v-model="highlight" @mousemove="$emit('update:highlight', site)" @mouseout="$emit('update:highlight', null)">
@@ -12,11 +12,11 @@
         </ul>
       </div>
 
-      <div v-if="tooManySites()">
+      <div v-if="tooManySites()" :class="direction==='horizontal'? 'col-lg-6 col-md-12 col-sm-12':''">
         <ul class="ToggleController">
-          <li v-for="(site, x) in sites.slice(5,10)">
+          <li v-for="(site, x) in sites.slice(5,10)"  >
             <label class="siteItem" v-model="highlight" @mousemove="$emit('update:highlight', site)" @mouseout="$emit('update:highlight', null)">
-              <span class="siteIcon" :class="site ==='Mean' ? 'Mean' : ''"  v-bind:style="{background:colors.slice(5,10)[x]}"></span>
+              <span class="siteIcon" v-if="site!==''" :class="site ==='Mean' ? 'Mean' : ''"  v-bind:style="{background:colors.slice(5,10)[x]}"></span>
               <span class="siteLabel">{{site ==="Mean" ? $t('meanTxt') : site}}</span>
             </label>
           </li>
@@ -37,6 +37,9 @@ export default {
       type: Array
     },
     highlight:{
+      type: String
+    },
+    direction:{
       type: String
     }
   },
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="mainContainer">
     <v-container>
       <div class="row">
+        <Legend class="col-12 row" v-bind:colors="colors" :sites="legendSites" :direction="'horizontal'"></Legend>
         <div v-for="set in sets" class="col-lg-6 col-md-12 col-sm-12">
           <BarChart style="height: 30vh" v-bind:colors="colors"  v-bind:data="data[set]" v-bind:title="set" autoresize></BarChart>
         </div>
@@ -13,14 +14,15 @@
 <script>
 import BarChart from "@/components/barChart";
 import Const from "@/const";
+import Legend from "@/components/legend";
 
 const mockData = [
-  ['','102','103','104','105'],
-  ['', 6, 4, 5, 7 ]]
+  ['category','101','102','103','104','105','106', 'total'],
+  ['',10, 6, 4, 5, 7, 3,25 ]]
 
 export default {
   name: "Home",
-  components: {BarChart},
+  components: {BarChart, Legend},
   async created() {
     this.data = {
       covid_cases: mockData,
@@ -29,11 +31,23 @@ export default {
       icu:mockData
     }
     },
+  computed:{
+    legendSites(){
+      var legendSites = mockData[0].slice(1,mockData[0].length);
+      if(legendSites.length<10 && legendSites.length >5){
+        while (legendSites.length < 11) {
+          legendSites.push('');
+        }
+      }
+      return legendSites;
+    },
+  },
   data() {
     return {
       sets:['covid_cases','death','ventilator','icu'],
       colors: Const.colors,
-      data:[]
+      data:[],
+      highlight: null
     }}
 }
 </script>
