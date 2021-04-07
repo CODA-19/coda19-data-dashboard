4d9c3cd285b31105b2818f3bc9acdb4fa4d42f93
Maxime Lavigne (malavv) <duguigne@gmail.com>
2021-04-06 21:02:45 -0400

    Modified 3 titles

--- front-end/src/api/AxiosInstance.js
+++ front-end/src/api/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: process.env.VUE_APP_CODA19_DASHBOARD_BACKEND_URL,
-    timeout: 9000,
+    timeout: 15000,
 });
 
 export default instance;
