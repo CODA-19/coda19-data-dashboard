0b59888eb46f91e920cd4a9b8905a9a7a7f89149
Maxime Lavigne (malavv) <duguigne@gmail.com>
2021-04-15 12:48:52 -0400

    Opacity of preview graph and updated date.

--- front-end/src/components/HomeTextTile.vue
+++ front-end/src/components/HomeTextTile.vue
@@ -4,7 +4,7 @@
             <h3 class="tileTitle">{{txTitle}}</h3>
             <div class="dataTileContent"><i18n-n :value="data"></i18n-n></div>
             <div class="nSitesTextTile">{{nSites}} Site</div>
-            <div class="bottomTextTile">{{txBottom}}</div>
+            <div class="bottomTextTile" style="opacity: 0.5;">{{txBottom}}</div>
         </div>
     </v-card>
 </template>
