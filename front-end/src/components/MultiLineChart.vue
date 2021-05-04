50b59de41d88cce8ca6fafd825d77b3baba50775
Maxime Lavigne (malavv) <duguigne@gmail.com>
2021-05-03 21:42:22 -0400

    Adding new LineChart allowing more type

---
+++ front-end/src/components/MultiLineChart.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-chart class="chart" ref="MultiLineChart" :option="option"/>
+</template>
+
+<script>
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import {LineChart} from "echarts/charts";
+import {
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+  ToolboxComponent
+} from "echarts/components";
+import VChart from "vue-echarts";
+import { zip } from "underscore";
+
+import TooltipLineFormatter from "./TooltipLineFormatter.vue";
+
+use([
+  CanvasRenderer,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+  ToolboxComponent,
+  LineChart
+]);
+
+import Vue from "vue";
+
+const hasCI = (dataSeries) => ["ll", "ul"].every(el => Object.keys(dataSeries).includes(el));
+
+export default {
+  name: "MultiLineChart",
+  components: { VChart },
+  props: {
+    data: Array,
+    dates: Array
+  },
+  methods: {
+    onResize() { this.$refs.MultiLineChart.resize(); },
+    getLineStyle(dataSeries) {
+      switch (dataSeries.type) {
+        case 'normal': return 'solid';
+        case 'predic': return 'dashed';
+        default: console.error(`unknown data series (${dataSeries.type}) to style`);
+      }
+    },
+    toCIStyle(dataSeries) {
+      if (!hasCI(dataSeries))
+        return [];
+
+      const name = dataSeries.name;
+      return [{
+        name: name,
+        type: 'line',
+        data: dataSeries.ll,
+        lineStyle: { opacity: 0 },
+        stack: name + 'ci',
+        symbol: 'none'
+      }, {
+        name: name,
+        type: 'line',
+        data: zip(dataSeries.ll, dataSeries.ul).map(arr => arr[1] - arr[0]),
+        lineStyle: { opacity: 0 },
+        areaStyle: { color: '#ccc'},
+        stack: name + 'ci',
+        symbol: 'none'
+      }];
+    },
+    toSeriesStyle(data) {
+      return [{
+        name: data.name,
+        type: 'line',
+        lineStyle: { type: this.getLineStyle(data) },
+        stack: '',
+        smooth: true,
+        data: data.est
+      }, ...this.toCIStyle(data)];
+    },
+    getTooltipFormatter() {
+      let ComponentClass = Vue.extend(TooltipLineFormatter);
+      return (series) => {
+        let instance = new ComponentClass({
+          propsData: {
+            date: series[0].name,
+            data: this.data,
+            series: series
+          }
+        });
+        instance.$mount();
+        return instance.$el;
+      };
+    }
+  },
+  mounted() { window.addEventListener("resize", this.onResize); },
+  beforeDestroy() { window.removeEventListener('resize', this.onResize); },
+  computed:{
+    option() {
+      return {
+        tooltip: {
+          trigger: 'axis', formatter: this.getTooltipFormatter(),
+        },
+        legend: { data: this.data.map(el => el.name) },
+        grid: { bottom: '3%', containLabel: true },
+        toolbox: { feature: { saveAsImage: {} } },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: this.dates.map(dateTime => dateTime.toFormat('LLL dd')) // Luxon Format
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: this.data.map(this.toSeriesStyle).flat()
+      };
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ front-end/src/components/TooltipLineFormatter.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <span>{{ date }}</span> <br>
+    <div style="display: flex; flex-direction: column;">
+      <div v-for="s in activeSeries" :key="s.name" style="display: flex; flex-direction: row;">
+        <div v-html="getLineMarker(s.name)"></div>
+        <div style="flex: 1;">
+          {{s.name}}
+          <sup v-if="s.predicted">pred</sup>
+        </div>
+        <div style="padding-left: 10px;">
+          {{s.value}}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "TooltipLineFormatter",
+  props: {
+    date: String,
+    data: Array,
+    series: Array
+  },
+  methods: {
+    /** Gets the legend line marker for the category with this name */
+    getLineMarker(name) { return this.series.filter(s => s.seriesName === name)[0].marker; },
+    /**
+     * Tries to guess if a data point comes from a prediction.
+     *
+     * @param ptIdx Index of the date in the tooltip
+     * @param pt Point being considered (with name, and value)
+     */
+    isPrediction(ptIdx, pt) {
+      const [name] = pt;
+      const ss = this.data.filter(d => d.name === name);
+      if (ss.length === 1 /* Simple line */ || ss.length === 3 /* with CI */)
+        return false;
+      // Confirms by checking the data series with data at that index is marked as a prediction.
+      return ss.filter(s => s.est[ptIdx])[0]?.type === "predic";
+    }
+  },
+  computed: {
+    activeSeries() {
+      let map = new Map();
+      for (let s of this.series)
+        map.set(s.seriesName, Math.max(s.value ?? -1, map.get(s.seriesName) ?? -1));
+
+      return Array.from(map.entries())
+          .map(e => ({name: e[0], value: e[1], predicted: this.isPrediction(this.series[0].dataIndex, e)}));
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
