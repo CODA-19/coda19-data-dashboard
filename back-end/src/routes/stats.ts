69f0531b3282f588f4441fd15335d34a1a07f097
Maxime Lavigne (malavv) <duguigne@gmail.com>
2021-05-18 16:44:23 -0400

    Added two mocked data packages.

--- back-end/src/routes/stats.ts
+++ back-end/src/routes/stats.ts
@@ -1,7 +1,28 @@
 import { Router, Request, Response } from 'express';
 import SummaryMeasures from "../../coda19-ts/src/request/SummaryMeasures";
+import {getCachedFile} from "../helpers/poly";
+import JSON5 from 'json5';
 
 const router = Router();
+
+router.get('/query_task_1', async function(req: Request, res: Response) {
+    try {
+        res.status(200).send(JSON5.parse(getCachedFile(req.app, './mock/tasks/ret_task_1.json5')));
+    } catch (err) {
+        console.error(err.stack);
+        res.status(500).send("Unable to fetch site info from Hub");
+    }
+})
+
+router.get('/query_task_2', async function(req: Request, res: Response) {
+    try {
+        res.status(200).send(JSON5.parse(getCachedFile(req.app, './mock/tasks/ret_task_2.json5')));
+    } catch (err) {
+        console.error(err.stack);
+        res.status(500).send("Unable to fetch site info from Hub");
+    }
+})
+
 
 router.get('/measures_v2', async function(req: Request, res: Response) {
     try {
