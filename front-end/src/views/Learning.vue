eca056fee586faf440ad50f33c2063aa894290f4
hamela <alexis.hamel@mail.mcgill.ca>
2021-05-21 11:09:22 -0400

    Learning - Change label over the video

--- front-end/src/views/Learning.vue
+++ front-end/src/views/Learning.vue
@@ -3,7 +3,7 @@
   <div class="learning">
      <h1>{{$t('learningTitle')}}</h1>
   </div>
-    <div>{{$t('startLearning')}}</div>
+    <div>{{$t('federated_simulation')}}</div>
     <div class="col-lg-8 offset-lg-2 col-md-12">
     <div class="videoWrapper">
       <iframe width="560" height="315" src="https://www.youtube.com/embed/4zZC7YjGNaE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
