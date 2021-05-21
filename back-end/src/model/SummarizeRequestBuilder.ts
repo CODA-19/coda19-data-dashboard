be179bace8a2a434755bd170a1b2b59bfb1fe03b
Maxime Lavigne (malavv) <duguigne@gmail.com>
2021-05-21 15:41:14 -0400

    With generic from and start for query 5-6

--- back-end/src/model/SummarizeRequestBuilder.ts
+++ back-end/src/model/SummarizeRequestBuilder.ts
@@ -68,6 +68,8 @@ export class SRSBld {
     }
 
     addTEMPORARYWardBreakdown(minDate: Date, maxDate: Date): SRSBld {
+        const fromDate = format(minDate, "yyyy-MM-dd");
+        const toDate = format(maxDate, "yyyy-MM-dd");
         this.selector.breakdown = {
             "resource": {
                 "type": "Encounter",
@@ -78,12 +80,14 @@ export class SRSBld {
                 "min": format(minDate, "yyyy-MM-dd"),
                 "max": format(maxDate, "yyyy-MM-dd")
             },
-            "query": "SELECT SQ.period_start as period_start, count(*) AS count_in_period FROM (SELECT DISTINCT resource->'subject'->>'reference' as subject_reference, to_timestamp(floor((extract('epoch' from (location->'period'->>'start')::timestamp) / 86400)) * 86400) AS period_start FROM Encounter encounter_table CROSS JOIN LATERAL jsonb_array_elements(resource -> 'location') AS location JOIN ( SELECT id FROM Location location_table CROSS JOIN LATERAL jsonb_array_elements(resource -> 'type') AS type CROSS JOIN LATERAL jsonb_array_elements(type -> 'coding') AS type_coding WHERE type_coding ->> 'code' = 'HU' ) location_table CROSS JOIN LATERAL jsonb_array_elements(encounter_table.resource -> 'location') AS location_location ON location_location -> 'location' ->> 'reference' = 'Location/' || location_table.id WHERE location -> 'period'->>'start'>='2021-01-01' AND location->'period'->>'start'<'2021-04-06') SQ GROUP BY period_start"
+            "query": `SELECT SQ.period_start as period_start, count(*) AS count_in_period FROM (SELECT DISTINCT resource->'subject'->>'reference' as subject_reference, to_timestamp(floor((extract('epoch' from (location->'period'->>'start')::timestamp) / 86400)) * 86400) AS period_start FROM Encounter encounter_table CROSS JOIN LATERAL jsonb_array_elements(resource -> 'location') AS location JOIN ( SELECT id FROM Location location_table CROSS JOIN LATERAL jsonb_array_elements(resource -> 'type') AS type CROSS JOIN LATERAL jsonb_array_elements(type -> 'coding') AS type_coding WHERE type_coding ->> 'code' = 'HU' ) location_table CROSS JOIN LATERAL jsonb_array_elements(encounter_table.resource -> 'location') AS location_location ON location_location -> 'location' ->> 'reference' = 'Location/' || location_table.id WHERE location -> 'period'->>'start'>='${fromDate}' AND location->'period'->>'start'<'${toDate}') SQ GROUP BY period_start`
         }
         return this;
     }
 
     addTEMPORARYIcuBreakdown(minDate: Date, maxDate: Date): SRSBld {
+        const fromDate = format(minDate, "yyyy-MM-dd");
+        const toDate = format(maxDate, "yyyy-MM-dd");
         this.selector.breakdown = {
             "resource": {
                 "type": "Encounter",
@@ -91,10 +95,10 @@ export class SRSBld {
             },
             "slices": {
                 "step": 24 * 60 * 60 /* Number of seconds in a day */ ,
-                "min": format(minDate, "yyyy-MM-dd"),
-                "max": format(maxDate, "yyyy-MM-dd")
+                "min": fromDate,
+                "max": toDate
             },
-            "query": "SELECT SQ.period_start as period_start, count(*) AS count_in_period FROM (SELECT DISTINCT resource->'subject'->>'reference' as subject_reference, to_timestamp(floor((extract('epoch' from (location->'period'->>'start')::timestamp) / 86400)) * 86400) AS period_start FROM Encounter encounter_table CROSS JOIN LATERAL jsonb_array_elements(resource -> 'location') AS location JOIN ( SELECT id FROM Location location_table CROSS JOIN LATERAL jsonb_array_elements(resource -> 'type') AS type CROSS JOIN LATERAL jsonb_array_elements(type -> 'coding') AS type_coding WHERE type_coding ->> 'code' = 'ICU' ) location_table CROSS JOIN LATERAL jsonb_array_elements(encounter_table.resource -> 'location') AS location_location ON location_location -> 'location' ->> 'reference' = 'Location/' || location_table.id WHERE location -> 'period'->>'start'>='2021-01-01' AND location->'period'->>'start'<'2021-04-06') SQ GROUP BY period_start"
+            "query": `SELECT SQ.period_start as period_start, count(*) AS count_in_period FROM (SELECT DISTINCT resource->'subject'->>'reference' as subject_reference, to_timestamp(floor((extract('epoch' from (location->'period'->>'start')::timestamp) / 86400)) * 86400) AS period_start FROM Encounter encounter_table CROSS JOIN LATERAL jsonb_array_elements(resource -> 'location') AS location JOIN ( SELECT id FROM Location location_table CROSS JOIN LATERAL jsonb_array_elements(resource -> 'type') AS type CROSS JOIN LATERAL jsonb_array_elements(type -> 'coding') AS type_coding WHERE type_coding ->> 'code' = 'ICU' ) location_table CROSS JOIN LATERAL jsonb_array_elements(encounter_table.resource -> 'location') AS location_location ON location_location -> 'location' ->> 'reference' = 'Location/' || location_table.id WHERE location -> 'period'->>'start'>='${fromDate}' AND location->'period'->>'start'<'${toDate}') SQ GROUP BY period_start`
         }
         return this;
     }
