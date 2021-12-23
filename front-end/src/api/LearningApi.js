c7f55236c340489ed09563450d0113ac1e5a46bb
arsenaultk9 <arsenaultk9@gmail.com>
2021-12-23 10:30:58 -0500

    Longer wait time + remove 110 dashboard temporarly.

--- front-end/src/api/LearningApi.js
+++ front-end/src/api/LearningApi.js
@@ -4,7 +4,7 @@ import TokenBearerHeaderFactory from './TokenBearerHeaderFactory';
 async function getPrepare(body, sitesUri) {
     const headers = TokenBearerHeaderFactory.get();
     try {
-        return await AxiosInstance.post(`learning/prepare?sites=${sitesUri}`, body, {headers: headers});
+        return await AxiosInstance.post(`learning/prepare?sites=${sitesUri}`, body, {headers: headers, timeout: 2*60*1000});
     } catch (err) {
         console.error(err.stack);
         res.status(500).send("Unable to run prepare query on backend");
