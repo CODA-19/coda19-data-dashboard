e5c6e807d8e0bd82bd1cee4dd1d5f4dabd239f76
Louis Mullie <louis.mullie@gmail.com>
2022-02-17 16:38:19 -0500

    Upgrade node-sass to fix build issues, import data dictionaries

--- front-end/README.md
+++ front-end/README.md
@@ -1,26 +1,32 @@
 # dashboard client
 
 # Deployment in production
- - cd ./front-end
- - docker login -u ${USER} -p ${USER}
- - ./publish.sh
- - Ask a VALERIA team member to update the image in openshift.
+
+- cd ./front-end
+- docker login -u ${USER} -p ${USER}
+- ./publish.sh
+- Ask a VALERIA team member to update the image in openshift.
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
-Launch on `localhost:8000` by default.
+
+Launch on `https://localhost:8000` by default.
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- front-end/src/control/SummaryFormFactory.js
+++ front-end/src/control/SummaryFormFactory.js
@@ -1,52 +1,71 @@
 import { sortBy } from "underscore";
 
+import {
+  ResourceTypes,
+  FiltersByDataType,
+  AttributesByResourceType,
+} from "@CODA-19/coda19-fhir-templates";
+
+console.log(ResourceTypes, FiltersByDataType, AttributesByResourceType);
 const secondsPerDay = 24 * 60 * 60;
 
 function qbRuleToFilter(rule) {
-    const { id, operator, value } = rule;
+  const { id, operator, value } = rule;
 
-    // FIXME(malavv): All of this is super temporary.
-    if (id === "deceasedBoolean" && operator === "equal") {
-        return { "path": "deceasedBoolean", "operator": "is", "value": value === 0 ? "false" : "true" }
-    }
-    if (id === "deceasedDateTime" && operator === "is_not_null") {
-        return { "path": "deceased.dateTime", "operator": "isNot", "value": "null" }
-    }
+  // FIXME(malavv): All of this is super temporary.
+  if (id === "deceasedBoolean" && operator === "equal") {
+    return {
+      path: "deceasedBoolean",
+      operator: "is",
+      value: value === 0 ? "false" : "true",
+    };
+  }
+  if (id === "deceasedDateTime" && operator === "is_not_null") {
+    return { path: "deceased.dateTime", operator: "isNot", value: "null" };
+  }
 
-    console.warn("Unknown rule, need to be managed.");
-    return rule;
+  console.warn("Unknown rule, need to be managed.");
+  return rule;
 }
 
 function qbBreakdown(cfg) {
-    const fd = cfg.resourceAttribute === 'deceasedDateTime' ? "deceased.dateTime" : cfg.resourceAttribute;
-    return {
-        "resource": { "type": cfg.resourceType, "field": fd },
-        "slices": { "step": Number.parseInt(cfg.period.step) * secondsPerDay, "min": cfg.period.start, "max": cfg.period.end }
-    }
+  const fd =
+    cfg.resourceAttribute === "deceasedDateTime"
+      ? "deceased.dateTime"
+      : cfg.resourceAttribute;
+  return {
+    resource: { type: cfg.resourceType, field: fd },
+    slices: {
+      step: Number.parseInt(cfg.period.step) * secondsPerDay,
+      min: cfg.period.start,
+      max: cfg.period.end,
+    },
+  };
 }
 
 export default class SummaryFormFactory {
-    static fromForm(dat, brkdwn) {
+  static fromForm(dat, brkdwn) {
+    const selector = {
+      resource: "Patient",
+      filters: dat.query.rules.map(qbRuleToFilter),
+      fields: sortBy(
+        dat.field.map((el) => ({ path: el.value })),
+        "path"
+      ),
+    };
 
-        const selector = {
-            "resource": "Patient",
-            "filters": dat.query.rules.map(qbRuleToFilter),
-            "fields": sortBy(dat.field.map(el => ({ path: el.value })), 'path')
-        };
-
-        if (brkdwn && dat.breakdown.resourceType !== "") {
-            selector['breakdown'] = qbBreakdown(dat.breakdown)
-        }
-
-        return {
-            "selectors": [ selector ],
-            "options": {
-                "measures": {
-                    "continuous": dat.measures.cont.map(el => el.value),
-                    "categorical": dat.measures.disc.map(el => el.value)
-                }
-            }
-        };
+    if (brkdwn && dat.breakdown.resourceType !== "") {
+      selector["breakdown"] = qbBreakdown(dat.breakdown);
     }
-}
 
+    return {
+      selectors: [selector],
+      options: {
+        measures: {
+          continuous: dat.measures.cont.map((el) => el.value),
+          categorical: dat.measures.disc.map((el) => el.value),
+        },
+      },
+    };
+  }
+}
